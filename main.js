4fdd2355df0354f61d66563dcdf8e45f2e7d76d9
amiluv <augustineug@gmail.com>
2023-11-24 19:46:43 +0100

    update

--- main.js
+++ main.js
@@ -62,56 +62,60 @@ function fetchCoinGeckoExchangeRates() {
         const amount = parseFloat(document.getElementById('amount').value);
         const fromCurrency = document.getElementById('from_currency').value;
         const toCurrency = document.getElementById('to_currency').value;
-    
-        fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${fromCurrency},${toCurrency}&vs_currencies=usd`)
-            .then(response => response.json())
-            .then(data => {
-                const fromRate = data[fromCurrency].usd;
-                const toRate = data[toCurrency].usd;
-                 const result = (amount / toRate) * fromRate;
-    
-                // Display the result and enable the Proceed button
-                const resultContainer = document.getElementById('result');
-                resultContainer.textContent = `You will get: ${result.toFixed(5)} ${toCurrency.toUpperCase()}`;
-                resultContainer.style.display = 'block';
-                document.getElementById('proceed-button').disabled = false;
-    
-                // Proceed button click event
-                document.getElementById('proceed-button').addEventListener('click', function() {
-                    const amount = parseFloat(document.getElementById('amount').value);
-                    const fromCurrency = document.getElementById('from_currency').value;
-                
-                    // Get wallet address based on the selected fromCurrency
-                    let walletAddress = ' ';
-                    if (fromCurrency === 'bitcoin') {
-                        walletAddress = 'bc1qsf946ej4thzae4xh76jxt4czwaeqjgushk0fdc';
-                    } else if (fromCurrency === 'ethereum') {
-                        walletAddress = '0x89c9D44Eb40876bb1F9A5cc30b0b7a0CA61A1E72';
-                    } else if (fromCurrency === 'tether') {
-                        walletAddress = 'TVb8VMB3iGGkSTHG8k7MrYvrcUTmnc7EPc';
-                    } else if (fromCurrency === 'tron') {
-                        walletAddress = 'TVb8VMB3iGGkSTHG8k7MrYvrcUTmnc7EPc';
-                    } else if (fromCurrency === 'binance-peg-busd') {
-                        walletAddress = '0x89c9D44Eb40876bb1F9A5cc30b0b7a0CA61A1E72';
-                    } else if (fromCurrency === 'binancecoin') {
-                        walletAddress = 'bnb1sqt5p8kv5yjss6u5s0sv3mkmntf88t7hpas86f';
-                    } else if (fromCurrency === 'dogecoin') {
-                        walletAddress = 'DMnchjQ9m7j5mhDDRAKofETbgZyyyWwivw';
-                    } else if (fromCurrency === 'cardano') {
-                        walletAddress = 'addr1q9hwgjwflx7wuykeh6tagpapsdy8r2cdnveclltf4vvawe4tq6npuceu9ql6vle4r68wjk8exldgxlmclrenrtvqd7yqmwful6';
-                    }else{
-                        return 'check your entries and try again!'
-                    }
-                    // You'd add more conditions for other currencies as needed
-                
-                    // Redirect to payment page with relevant details
-                    const paymentURL = `payment_page.html?amount=${amount}&currency=${fromCurrency}&address=${walletAddress}`;
-                    window.location.href = paymentURL;
+
+        
+            fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${fromCurrency},${toCurrency}&vs_currencies=usd`)
+                .then(response => response.json())
+                .then(data => {
+                    const fromRate = data[fromCurrency].usd;
+                    const toRate = data[toCurrency].usd;
+                    const result = (amount / toRate) * fromRate;
+        
+                    // Display the result and enable the Proceed button
+                    const resultContainer = document.getElementById('result');
+                    resultContainer.textContent = `You will get: ${result.toFixed(5)} ${toCurrency.toUpperCase()}`;
+                    resultContainer.style.display = 'block';
+                    document.getElementById('proceed-button').disabled = false;
+        
+                    // Proceed button click event
+                    document.getElementById('proceed-button').addEventListener('click', function() {
+                        const amount = parseFloat(document.getElementById('amount').value);
+                        const fromCurrency = document.getElementById('from_currency').value;
+                    
+                        // Get wallet address based on the selected fromCurrency
+                        let walletAddress = ' ';
+                        if (fromCurrency === 'bitcoin') {
+                            walletAddress = 'bc1qsf946ej4thzae4xh76jxt4czwaeqjgushk0fdc';
+                        } else if (fromCurrency === 'ethereum') {
+                            walletAddress = '0x89c9D44Eb40876bb1F9A5cc30b0b7a0CA61A1E72';
+                        } else if (fromCurrency === 'tether') {
+                            walletAddress = 'TVb8VMB3iGGkSTHG8k7MrYvrcUTmnc7EPc';
+                        } else if (fromCurrency === 'tron') {
+                            walletAddress = 'TVb8VMB3iGGkSTHG8k7MrYvrcUTmnc7EPc';
+                        } else if (fromCurrency === 'usd-coin') {
+                            walletAddress = 'U41039047';
+                        } else if (fromCurrency === 'binance-peg-busd') {
+                            walletAddress = '0x89c9D44Eb40876bb1F9A5cc30b0b7a0CA61A1E72';
+                        } else if (fromCurrency === 'binancecoin') {
+                            walletAddress = 'bnb1sqt5p8kv5yjss6u5s0sv3mkmntf88t7hpas86f';
+                        } else if (fromCurrency === 'dogecoin') {
+                            walletAddress = 'DMnchjQ9m7j5mhDDRAKofETbgZyyyWwivw';
+                        } else if (fromCurrency === 'cardano') {
+                            walletAddress = 'addr1q9hwgjwflx7wuykeh6tagpapsdy8r2cdnveclltf4vvawe4tq6npuceu9ql6vle4r68wjk8exldgxlmclrenrtvqd7yqmwful6';
+                        }else{
+                            return 'check your entries and try again!'
+                        }
+                        // You'd add more conditions for other currencies as needed
+                    
+                        // Redirect to payment page with relevant details
+                        const paymentURL = `payment_page.html?amount=${amount}&currency=${fromCurrency}&address=${walletAddress}`;
+                        window.location.href = paymentURL;
+                    });
+                    
+                })
+                .catch(error => {
+                console.error('Error fetching exchange rates from CoinGecko:', error);
                 });
-                
-            })
-            .catch(error => {
-             console.error('Error fetching exchange rates from CoinGecko:', error);
-            });
+            
     
     });
